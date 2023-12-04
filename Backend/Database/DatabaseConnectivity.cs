b24fd4e5c676c22362e0e8ec778d6ec7b34be05f
Danny <dcrespin@student.touro.edu>
2023-12-04 18:35:46 -0500

    Brought in some old code from the previous Repo. User class to represent a user with all his info along with all of the Shuls he follows- represented by the Shul class. Additionally, the Database connectivity code to connect to the DB has been added as well.

--- Backend/Database/DatabaseConnectivity.cs
+++ Backend/Database/DatabaseConnectivity.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -7,21 +8,59 @@ using System.Data.SqlClient;
 
 namespace Super_Jew_2._0.Backend.Database
 {
-    public class DatabaseConnectivity
+    public class DataBaseConnectivity
     {
-        private string connectionString;
+        
+        // private var databaseConnectionString = new ConfigurationBuilder()
+        //     .AddUserSecrets<PackagePilotContext>(true)
+        //     .Build()["DatabaseConnectionString"];
+        private string _connectionString = "server=localhost;port=3306;database=myDatabase;user=root;password=myPassword;";
 
-        public DatabaseConnectivity(string server, string database)
+        // Set your database connection string here
+
+        
+
+        public  User? GetUserByPassword(string username, string password)
         {
-            connectionString = "Server = {server}; Database = {database}; Trusted_Connection = True";
-        }
+            User? user = null;
 
-        public SqlConnection ConnectToDatabase()
-        {
-            SqlConnection connection = new SqlConnection(connectionString);
-            connection.Open();
-            return connection;
-        }
+            using var connection = new MySqlConnection(_connectionString);
+            using (var command = new MySqlCommand("GetUserByPassword", connection))
+            {
+                command.CommandType = CommandType.StoredProcedure;
+                command.Parameters.AddWithValue("inputUsername", username);
+                command.Parameters.AddWithValue("inputPassword", password);
 
+                connection.Open();
+                using var reader = command.ExecuteReader();
+                while (reader.Read())
+                {
+                    user ??= new User
+                    {
+                        UserID = reader.GetInt32("UserID"),
+                        Username = reader.GetString("Username"),
+                        DateOfBirth = reader.GetDateTime("DateOfBirth"),
+                        ReligiousDenomination = reader.GetString("ReligiousDenomination"),
+                        AccountType = reader.GetString("AccountType")
+                    };
+
+                    var shul = new Shul
+                    {
+                        ShulID = reader.GetInt32("ShulID"),
+                        ShulName = reader.GetString("Name"),
+                        Location = reader.GetString("Location"),
+                        Denomination = reader.GetString("Denomination"),
+                        ContactInfo = reader.GetString("ContactInfo"),
+                        ShachrisTime = reader.GetString("ShachrisTime"),
+                        MinchaTime = reader.GetString("MinchaTime"),
+                        MaarivTime = reader.GetString("MaarivTime")
+                    };
+
+                    user.FollowedShuls.Add(shul);
+                }
+
+                return user;
+            }
+        }
     }
 }
---
+++ Backend/User.cs
@@ -0,0 +1,15 @@
+namespace Super_Jew_2._0.Backend;
+
+public class User
+{
+    public int UserID { get; set; }
+    public string Username { get; set; }
+    public DateTime DateOfBirth { get; set; }
+    public string ReligiousDenomination { get; set; }
+    public string AccountType { get; set; }
+    public List<Shul> FollowedShuls { get; set; }
+
+   
+    
+    
+}
