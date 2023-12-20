19940b4efc05097aed378ba4abdf92b31721a939
Yonatan <ykatz8@student.touro.edu>
2023-12-19 19:12:27 -0500

    log in works for user with data not hard coded

--- Backend/ShulServices/UserService.cs
+++ Backend/ShulServices/UserService.cs
@@ -3,12 +3,19 @@ namespace Super_Jew_2._0.Backend.ShulServices
     public class UserService
     {
         public User CurrentUser { get; private set; }
+        
+        public string UserPassword { get; private set; }
         public event Action OnUserChanged;
 
         public void SetCurrentUser(User user)
         {
             CurrentUser = user;
             OnUserChanged?.Invoke();
+        }
+        
+        public void SetPassword(string password)
+        {
+            UserPassword = password;
         }
 
         public void Logout()
