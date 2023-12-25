251c6eec4aca25aad2c473ee21aa812e614c602f
Yonatan <ykatz8@student.touro.edu>
2023-12-25 17:42:01 -0500

    Minor events additions

---
+++ Backend/Event.cs
@@ -0,0 +1,11 @@
+namespace Super_Jew_2._0.Backend;
+
+public class Event
+{
+    public int EventID { get; set; }
+    public int ShulId { get; set; }
+    public string EventName { get; set; }
+    public string TimeOfEvent { get; set; }
+    public string Location { get; set; }
+    public string Subscription { get; set; }
+}
