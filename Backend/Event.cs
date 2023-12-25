5950a7c8ff4ffb827c8b90fd16d992b3e8bd052b
kimmel25 <dovikimmel@gmail.com>
2023-12-25 14:58:22 -0500

    events class

---
+++ Backend/Event.cs
@@ -0,0 +1,12 @@
+﻿namespace Super_Jew_2._0.Backend
+{
+    public class Event
+    {
+        public int EventID { get; set; }
+        public int ShulID { get; set; }
+        public string EventName { get; set; }
+        public string TimeOfEvent { get; set; }
+        public string Location { get; set; }
+        public string Subscription { get; set; }
+    }
+}
