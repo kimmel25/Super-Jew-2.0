8d246b9dbc5bb79f8d4d0942f2b9a315b3ae554f
Danny <dcrespin@student.touro.edu>
2023-12-26 01:26:42 -0500

    Created a method which turns a regular user into a Gabbai

--- Backend/Database/DatabaseConnectivity.cs
+++ Backend/Database/DatabaseConnectivity.cs
@@ -209,9 +209,27 @@ namespace Super_Jew_2._0.Backend.Database
                 return result > 0; // returns true if it affected at least one record
             }
         }
-
-
         
+        
+
+        public static bool UpgradeUserToGabbai(int userId)
+        {
+            using var connection = new MySqlConnection(ConnectionString);
+            using (var command = new MySqlCommand("UpgradeUserToGabbai", connection))
+            {
+                command.CommandType = CommandType.StoredProcedure;
+                command.Parameters.AddWithValue("userId", userId);
+                connection.Open();
+                using var reader = command.ExecuteReader();
+                var result = command.ExecuteNonQuery();
+                return result > 0;
+                
+
+            }
+        }
+
+
+
         //Methods for Gabbais Only!
         
         //this should function similar to AddShulToUser. procedure is created.
